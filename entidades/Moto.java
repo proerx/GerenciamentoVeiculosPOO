6ed501593fe2c30523cc0f0abcfb99d0f8761ef3
proerx <132977178+proerx@users.noreply.github.com>
2025-02-11 15:32:32 -0300

    Moto.java

--- entidades/Moto.java
+++ entidades/Moto.java
@@ -1,16 +1,26 @@
 package entidades;
 
-public class Moto extends Veiculo {
+public class Moto extends Veiculo {]
+    private int cilindradas;
     private double consumoCombustivel;
 
-    public Moto(String marca, String modelo, int ano, double quilometragem, double consumoCombustivel) {
+    public Moto(String marca, String modelo, int ano, double quilometragem, double consumoCombustivel, int cilindradas) {
         super(marca, modelo, ano, quilometragem);
         this.consumoCombustivel = consumoCombustivel;
+        this.cilindradas = cilindradas;
     }
 
     @Override
     public double calcularConsumo() {
         return consumoCombustivel;
+    }
+
+    public int getCilindradas() {
+        return cilindradas;
+    }
+    
+    public void exibirDetalhes() {
+        System.out.println("Cilindradas: " + cilindradas);
     }
      
 }
