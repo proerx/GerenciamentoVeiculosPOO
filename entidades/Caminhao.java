14feda361dff0ca591d6745e5571b4b3432c5db2
proerx <132977178+proerx@users.noreply.github.com>
2025-02-11 15:34:29 -0300

    Caminhao.java

--- entidades/Caminhao.java
+++ entidades/Caminhao.java
@@ -1,16 +1,26 @@
 package entidades;
 
 public class Caminhao extends Veiculo {
+    private double capacidadeCarga;
     private double consumoCombustivel;;
 
-    public Caminhao(String marca, String modelo, int ano, double quilometragem, double consumoCombustivel) {
+    public Caminhao(String marca, String modelo, int ano, double quilometragem, double consumoCombustivel, double capacidadeCarga) {
         super(marca, modelo, ano, quilometragem);
         this.consumoCombustivel = consumoCombustivel;
+        this.capacidadeCarga = capacidadeCarga;
     }
     
     @Override
     public double calcularConsumo() {
         return consumoCombustivel;
     }
- 
+    
+    public double getCapacidadeCarga() {
+        return capacidadeCarga;
+    }
+    
+    public void exibirDetalhes() {
+        System.out.println("Capacidade de carga: " + capacidadeCarga + " toneladas");
+    }
+    
 }
