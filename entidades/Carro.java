8c5a7100d894aa1f541273c85ba240990afd0d74
proerx <132977178+proerx@users.noreply.github.com>
2025-02-11 15:31:12 -0300

    Carro.java

--- entidades/Carro.java
+++ entidades/Carro.java
@@ -1,16 +1,26 @@
 package entidades;
 
 public class Carro extends Veiculo {
+    private boolean temArCondicionado;
     private double consumoCombustivel;
 
-    public Carro(String marca, String modelo, int ano, double quilometragem, double consumoCombustivel) {
+    public Carro(String marca, String modelo, int ano, double quilometragem, double consumoCombustivel, boolean temArCondicionado) {
         super(marca, modelo, ano, quilometragem);
         this.consumoCombustivel = consumoCombustivel;
+        this.temArCondicionado = temArCondicionado;
     }
 
     @Override
     public double calcularConsumo() {
         return 10.0;
+    }
+
+    public boolean isTemArCondicionado() {
+        return temArCondicionado;
+    }
+
+    public void exibirDetalhes() {
+        System.out.println("Tem ar-condicionado: " + (temArCondicionado ? "Sim" : "Não"));
     }
  
 }
